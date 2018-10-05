abb5c065d6f9e7a38a8cf375252d1c6957312850
John Doe <johndoe@example.com>
2018-10-05 14:44:59 +0300

    add project

---
+++ mean-app/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+
+export class PostCreateComponent {
+  enteredTitle = '';
+  enteredContent = '';
+  postsService: PostsService;
+
+  constructor(postsService: PostsService) {
+    this.postsService = postsService;
+  }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.reset();
+  }
+}
+
---
+++ mean-app/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  postsService: PostsService;
+  private postsSub: Subscription;
+
+  constructor(postsService: PostsService) {
+    this.postsService = postsService;
+  }
+
+  ngOnInit() {
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  onDelete(postId: string) {
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
