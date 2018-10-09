9e779cf55f92ca656bd0a330d8194c9efcda1b0a
John Doe <johndoe@example.com>
2018-10-09 17:57:06 +0300

    enhancing the app in progess

--- mean-app/src/app/app.module.ts
+++ mean-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import {
   } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
@@ -26,6 +27,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
